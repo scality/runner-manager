46ab12df047fcb1a8343b0bd1239a1bfe0090b95
Erwan Bernard <erwan.bernard079@gmail.com>
2021-11-17 14:44:05 +0100

    [RELENG-5152] Create type for webhooks data

--- srcs/web/WebhookManager.py
+++ srcs/web/WebhookManager.py
@@ -1,15 +1,16 @@
 import logging
 
+from web.models import WebHook
 from . import runner_m
 
 logger = logging.getLogger("runner_manager")
 
 
-class Webhook(object):
+class WebHookManager(object):
     event: str
-    payload: dict
+    payload: WebHook
 
-    def __init__(self, payload: dict, event: str):
+    def __init__(self, payload: WebHook, event: str):
         self.event = event
         self.payload = payload
 
@@ -21,30 +22,32 @@ class Webhook(object):
             logger.info(f'Get event: {self.event}')
             getattr(self, self.event)(self.payload)
 
-    def workflow_run(self, payload):
+    def workflow_run(self, payload: WebHook):
         pass
 
-    def workflow_job(self, payload):
+    def workflow_job(self, payload: WebHook):
         status = {}
-        if payload['action'] == 'queued' or "self-hosted" not in payload["workflow_job"]["labels"]:
+        if (payload.action == 'queued'
+                or "self-hosted" not in payload.workflow_job.labels
+                or payload.workflow_job.runner_id is None):
             return
-        elif payload['action'] == 'in_progress':
+        elif payload.action == 'in_progress':
             status = {
                 'status': 'online',
                 'busy': True,
             }
-        elif payload['action'] == 'completed':
+        elif payload.action == 'completed':
             status = {
                 'status': 'offline',
                 'busy': False,
             }
 
         status.update({
-            'name': payload["workflow_job"]["runner_name"],
-            'id': payload["workflow_job"]["runner_id"],
-            'labels': payload["workflow_job"]["labels"],
+            'name': payload.workflow_job.runner_name,
+            'id': payload.workflow_job.runner_id,
+            'labels': payload.workflow_job.labels,
         })
         runner_m.update_runner_status(status)
 
-    def ping(self, payload):
+    def ping(self, payload: WebHook):
         logger.info('Ping from Github')
