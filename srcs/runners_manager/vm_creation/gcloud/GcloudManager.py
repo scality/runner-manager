b85bfd1aa6a8e7f949b27a2640170a0b2a8f3209
Thomas Carmet <thomas.carmet@scality.com>
2022-06-07 23:01:02 -0700

    RELENG-6105 add necessary scopes to run gcloud ops agent

--- srcs/runners_manager/vm_creation/gcloud/GcloudManager.py
+++ srcs/runners_manager/vm_creation/gcloud/GcloudManager.py
@@ -101,6 +101,8 @@ class GcloudManager(CloudManager):
                     scopes=[
                         "https://www.googleapis.com/auth/devstorage.read_write",
                         "https://www.googleapis.com/auth/logging.write",
+                        "https://www.googleapis.com/auth/monitoring.write",
+                        "https://www.googleapis.com/auth/cloud-platform",
                     ],
                 )
             ],
