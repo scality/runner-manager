e62a8f85f6a642320d8fde65621dbaacbc61aeb3
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-02-14 15:19:46 -0800

    Runner-manager for AWS EC2 instances (#171)
    Co-authored-by: Thomas Carmet <thomas.carmet@scality.com>

---
+++ docs/setup/clouds/aws.md
@@ -0,0 +1,55 @@
+# AWS
+
+The runner have the capability to create resources on AWS. Follow the steps below to authenticate
+and configure this service
+
+
+## Credentials
+
+Set the following environment variables, with your respective AWS credentials:
+
+* `AWS_ACCESS_KEY_ID`
+* `AWS_SECRET_ACCESS_KEY`
+* `AWS_SESSION_TOKEN` (Optional)
+
+## Config
+
+### AWS Region
+
+Configure the region where EC2 instances will be created with the `AWS_DEFAULT_REGION` environment variable. Per example `us-west-2`.
+
+### Service
+
+Considering the application credentials has been setup on the service system,
+the remaining service configuration you'll need are:
+
+```yaml
+cloud_name: 'aws'
+cloud_config: {}
+```
+
+### Runner
+Here's an example of a pool of runner on aws running with Ubuntu (image_id corresponding to it):
+```yaml
+runner_pool:
+  - config:
+      image_id: 'ami-0735c191cf914754d'
+      instance_type: 't2.micro'
+      security_group_ids:
+        - 'sg-XXX'
+        - 'sg-XXX'
+      subnet_id: 'subnet-XXX'
+      disk_size_gb: 30
+    quantity:
+      min: 2
+      max: 4
+    tags:
+      - aws
+      - ubuntu
+      - jammy
+      - small
+```
+
+* As for the information and characteristics to be filled in, you can find them in the [AWS management console].
+
+[AWS management console]: https://us-west-2.console.aws.amazon.com/console/home?region=us-west-2#
---
+++ srcs/runners_manager/vm_creation/aws/AwsManager.py
@@ -0,0 +1,134 @@
+import logging
+import boto3
+
+from runners_manager.monitoring.prometheus import metrics
+from runners_manager.runner.Runner import Runner
+from runners_manager.runner.Runner import VmType
+from runners_manager.vm_creation.CloudManager import CloudManager
+from runners_manager.vm_creation.CloudManager import create_vm_metric
+from runners_manager.vm_creation.CloudManager import delete_vm_metric
+
+from runners_manager.vm_creation.aws.schema import AwsConfigVmType
+
+logger = logging.getLogger("runner_manager")
+
+
+class AwsManager(CloudManager):
+    CONFIG_VM_TYPE_SCHEMA = AwsConfigVmType
+
+    def __init__(
+        self,
+        name: str,
+        settings: dict,
+        redhat_username: str,
+        redhat_password: str,
+        ssh_keys: str,
+    ):
+        super(AwsManager, self).__init__(
+            name, settings, redhat_username, redhat_password, ssh_keys
+        )
+        self.ec2 = boto3.client("ec2")
+
+    def delete_existing_runner(self, runner: Runner):
+        """Delete an old runner instance from AWS if it exists."""
+
+        for listed_runner in self.get_all_vms(runner.name):
+            if runner.name == listed_runner.name:
+                logger.info(f"Found an existing instance of {runner.name}")
+                return self.delete_vm(listed_runner)
+        logger.info(f"No existing instance for runner {runner.name} has been found")
+        return None
+
+    @create_vm_metric
+    def create_vm(
+        self,
+        runner: Runner,
+        runner_token: int or None,
+        github_organization: str,
+        installer: str,
+        call_number=0,
+    ):
+        try:
+            logger.info(f"Creating {runner.name} instance")
+
+            user_data = self.script_init_runner(
+                runner, runner_token, github_organization, installer
+            )
+
+            instance = self.ec2.run_instances(
+                ImageId=runner.vm_type.config["image_id"],
+                InstanceType=runner.vm_type.config["instance_type"],
+                SecurityGroupIds=runner.vm_type.config["security_group_ids"],
+                SubnetId=runner.vm_type.config["subnet_id"],
+                MaxCount=1,
+                MinCount=1,
+                UserData=user_data,
+                BlockDeviceMappings=[
+                    {
+                        'DeviceName': '/dev/sda1',
+                        'Ebs': {
+                            'VolumeSize': int(runner.vm_type.config["disk_size_gb"]),
+                            'DeleteOnTermination': True,
+                            'VolumeType': 'gp2'
+                        },
+                    },
+                ],
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': runner.name
+                            },
+                            {
+                                'Key': 'Runner-manager',
+                                'Value': 'true'
+                            }
+                        ]
+                    },
+                ],
+            )
+
+            return instance['Instances'][0]['InstanceId']
+
+        except Exception as exception:
+            metrics.runner_creation_failed.labels(cloud=self.name).inc()
+            logger.error(exception)
+            raise exception
+
+    @delete_vm_metric
+    def delete_vm(self, runner):
+        try:
+            self.ec2.terminate_instances(InstanceIds=[runner.vm_id])
+            logger.info(f"Instance of runner {runner.name} has been terminated")
+
+        except Exception as exception:
+            logger.info(f"Instance of runner {runner.name} {exception}")
+            pass
+
+    def get_all_vms(self, prefix: str) -> list[Runner]:
+        # Get all vms in ec2
+        runners = []
+
+        for reservation in self.ec2.describe_instances()['Reservations']:
+            for instance in reservation['Instances']:
+                for tag in instance.get('Tags', []):
+                    if tag.get('Key') == 'Name' and tag.get('Value').startswith(prefix):
+                        runner = Runner(
+                            tag.get('Value'),
+                            instance.get('InstanceId'),
+                            VmType(
+                                {
+                                    "tags": [],
+                                    "config": {},
+                                    "quantity": {},
+                                }
+                            ),
+                            self.name
+                        )
+                        runners.append(runner)
+            return runners
+
+    def delete_images_from_shelved(self, name):
+        logger.info(f"nothing to do for {name}")
