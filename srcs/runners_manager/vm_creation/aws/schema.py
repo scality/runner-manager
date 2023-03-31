28b96635e3b12a51e9c89b2b09166bfafe64db97
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-03-31 11:45:45 -0500

    Update schema.py

--- srcs/runners_manager/vm_creation/aws/schema.py
+++ srcs/runners_manager/vm_creation/aws/schema.py
@@ -1,7 +1,9 @@
 from marshmallow import Schema, fields
 
+
 class AwsCloudConfig(Schema):
     tags = fields.Dict(keys=fields.Str(), values=fields.Str())
+
 
 class AwsConfigVmType(Schema):
     image_id = fields.Str(required=True)
