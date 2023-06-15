5a98747040eaf9cfb118c1e82dff4318e653a76d
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-06-15 16:53:03 -0700

    Update srcs/runners_manager/vm_creation/gcloud/GcloudManager.py
    Co-authored-by: Thomas Carmet <thomas.carmet@scality.com>

--- srcs/runners_manager/vm_creation/gcloud/GcloudManager.py
+++ srcs/runners_manager/vm_creation/gcloud/GcloudManager.py
@@ -74,7 +74,7 @@ class GcloudManager(CloudManager):
                 if len(value) > 63:
                     value = value[:63]
                 value = value.lower()
-                value = ''.join(c if c.islower() or c.isdigit() or c in ['_', '-'] else '_' for c in value)
+                value = ''.join(c if c.islower() or c.isdigit() or c in ['_', '-'] else '-' for c in value)
                 labels[key] = value
 
             instance.labels = labels
