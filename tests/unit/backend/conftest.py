47c349ac18b70d0ea416137f0fb7bb2278e4a861
Thomas Carmet <thomas.carmet@scality.com>
2023-08-01 21:47:28 +0000

    give backends.base as param

--- tests/unit/backend/conftest.py
+++ tests/unit/backend/conftest.py
@@ -1,9 +1,10 @@
 from pytest import fixture
 
 from runner_manager.backend.base import BaseBackend
+from runner_manager.models.backend import Backends
 
 
 @fixture()
 def backend() -> BaseBackend:
     """Fixture for backend."""
-    return BaseBackend.get_backend(name="base", config=None)
+    return BaseBackend.get_backend(name=Backends.base, config=None)
