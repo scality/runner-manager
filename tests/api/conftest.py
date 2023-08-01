8ad6f9aadaa889ae16b603c6809198615a499afb
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-08-01 15:44:01 +0000

    [PTFE-669] ✨ Bootstrap API Tests

---
+++ tests/api/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from runner_manager.main import app
+
+@pytest.fixture(scope="function")
+def fastapp():
+    fastapp = app
+    fastapp.dependency_overrides = {}
+    return fastapp
+
+
+@pytest.fixture(scope="function")
+def client(fastapp):
+    client = TestClient(fastapp)
+    return client
