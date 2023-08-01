43de0f1d5a3e8e18172f753406f418db839f5b74
gaspardmoindrot <gaspard.moindrot@scality.com>
2023-08-01 18:36:56 +0000

    [PTFE-680] ✨ Support webhook endpoint

---
+++ tests/api/test_job.py
@@ -0,0 +1,12 @@
+def test_webhook_job_endpoint(client):
+    headers = {'X-GitHub-Event': 'push'}
+    webhook = {
+        'repository': {
+            'name': 'repo',
+            'full_name': 'org/repo',
+            'visibility': 'private',
+        }
+    }
+    response = client.post("/webhook", json=webhook, headers=headers)
+    
+    assert response.status_code == 202
