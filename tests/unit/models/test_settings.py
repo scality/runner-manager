2e421d3b8e538792227a141914e5156060e4e005
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-02 12:23:05 +0000

    testing

--- tests/unit/models/test_settings.py
+++ tests/unit/models/test_settings.py
@@ -10,12 +10,4 @@ def test_settings_default_values():
 
 
 
-def test_settings_override_values():
-    redis_url = RedisDsn.build(scheme="redis", host="localhost", port="6379")
-    github_url = AnyHttpUrl.build(scheme="https", host="github.com", port="443")
-    settings = Settings(name="test", redis_om_url=redis_url, github_base_url=github_url)
-    assert settings.name == "test"
-    assert settings.redis_om_url == redis_url
-    assert settings.github_base_url == github_url
-
 
