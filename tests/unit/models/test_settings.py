ab12bf98b7a97c78adccc9312693ad3225d41172
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-02 15:00:37 +0000

    testing

--- tests/unit/models/test_settings.py
+++ tests/unit/models/test_settings.py
@@ -1,15 +1,22 @@
 from runner_manager.models.settings import Settings
-from runner_manager.models.settings import yaml_config_settings_source
 from pytest import fixture
-import tempfile
-import os
-import yaml
-from pathlib import Path
 
 
-@fixture
 def test_settings_default_values():
     settings = Settings()
     assert settings.name == "runner-manager"
-    assert settings.redis_om_url is None
-    assert settings.github_base_url is None
+    assert settings.redis_om_url == "redis://localhost:6379/0"
+    assert settings.github_base_url == "http://localhost:4010"
+
+@fixture
+def test_env_settings():
+    env_settings = {
+        "NAME": "name-test",
+        "REDIS_OM_URL": "redis://localhost:6379/1",
+        "GITHUB_BASE_URL": "http://localhost:4999",
+    }
+    settings = Settings()
+    settings.Config.customise_sources(env_settings=env_settings, init_settings={}, file_secret_settings={})
+    assert settings.name == "name-test"
+    assert settings.redis_om_url == "redis://localhost:6379/1"
+    assert settings.github_base_url == "http://localhost:4999"
