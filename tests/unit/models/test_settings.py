b065726263a2b8e800a2dfed5c829440937a2774
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-02 12:56:28 +0000

    PTFE-693: env file settings test

--- tests/unit/models/test_settings.py
+++ tests/unit/models/test_settings.py
@@ -1,5 +1,5 @@
 from runner_manager.models.settings import Settings
-from pydantic import AnyHttpUrl, RedisDsn
+
 
 
 def test_settings_default_values():
@@ -8,6 +8,19 @@ def test_settings_default_values():
     assert settings.redis_om_url == "redis://localhost:6379/0"
     assert settings.github_base_url == "http://localhost:4010"
 
+
+def test_env_settings():
+    env_settings = {
+        "NAME": "name-test",
+        "REDIS_OM_URL": "redis://localhost:6379/1",
+        "GITHUB_BASE_URL": "http://localhost:4999",
+    }
+    settings = Settings()
+    settings.Config.customise_sources(env_settings=env_settings, init_settings={},
+                                       file_secret_settings={})
+    assert settings.name == "name-test"
+    assert settings.redis_om_url == "redis://localhost:6379/1"
+    assert settings.github_base_url == "http://localhost:4999"
 
 
 
