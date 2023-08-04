e3894d1fe74fc01a9ca5552615c5b45e64cba2d6
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-04 15:57:20 +0000

    PTFE-672: added test for api

---
+++ tests/api/auth_tests.py
@@ -0,0 +1,21 @@
+import os
+
+from runner_manager.models.settings import Settings
+
+
+def test_public_endpoint(client):
+    response = client.get("/public")
+
+    assert response.status_code == 200
+
+
+def test_private_endpoint_without_api_key(client):
+    response = client.get("/private")
+    assert response.status_code == 401
+
+
+def test_private_endpoint_with_valid_api_key(client):
+    os.environ["API_KEY"] = "secret"
+    headers = {"x-api-key": "secret"}
+    response = client.get("/private", headers=headers)
+    assert response.status_code == 200
