65835c956b1b378b1da90a4de9c1cf912fc0483f
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-04 07:53:51 +0000

    api authentication

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -1,7 +1,10 @@
+import os
+
 import pytest
 from fastapi.testclient import TestClient
 
 from runner_manager.main import app
+from runner_manager.models.settings import Settings
 
 
 @pytest.fixture(scope="function")
@@ -15,3 +18,22 @@ def fastapp():
 def client(fastapp):
     client = TestClient(fastapp)
     return client
+
+
+def test_public_endpoint(client):
+    response = client.get("/public")
+
+    assert response.status_code == 200
+
+
+def test_private_endpoint_without_api_key(client):
+    response = client.get("/private")
+    assert response.status_code == 401
+
+
+def test_private_endpoint_with_valid_api_key(client):
+    os.environ["API_KEY"] = "secret"
+    settings = Settings()
+    headers = {"x-api-key": settings.API_KEY}
+    response = client.get("/private", headers=headers)
+    assert response.status_code == 200
