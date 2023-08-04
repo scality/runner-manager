f49805fbc52b2c2b6577d07cd5508e2e5a821a70
Thomas Carmet <thomas.carmet@scality.com>
2023-08-04 22:37:01 +0000

    fix test settings

--- tests/unit/models/test_settings.py
+++ tests/unit/models/test_settings.py
@@ -1,45 +1,52 @@
 import os
 import tempfile
+
 import pytest
 import yaml
 from pytest import fixture
 
-from runner_manager.models.settings import Settings
+from runner_manager.dependencies import get_settings
+from runner_manager.models.settings import ConfigFile, Settings
+
+
+@fixture
+def yaml_data():
+    return {
+        "name": "test-runner-manager",
+        "redis_om_url": "redis://localhost:6379/0",
+        "github_base_url": "https://github.com",
+    }
+
+
+@fixture(scope="function")
+def config_file(yaml_data):
+    # create a yaml file with some data
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
+        yaml.dump(yaml_data, f)
+        os.environ["CONFIG_FILE"] = f.name
+        config = ConfigFile()
+        return config.config_file
 
 
 def test_settings_default_values():
     settings = Settings()
     assert settings.name == "runner-manager"
-    assert settings.redis_om_url == os.getenv("REDIS_OM_URL", "redis://localhost:6379/0")
+    assert settings.redis_om_url == os.getenv("REDIS_OM_URL")
     assert settings.github_base_url == os.getenv("GITHUB_BASE_URL")
 
-
-@fixture
-def temp_yaml_file():
-    yaml_data = """
-    name: test-runner-manager
-    redis_om_url: redis://localhost:6379/0
-    github_base_url: https://github.com
-    """
-    yaml_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
-    yaml_file.write(yaml_data)
-    return yaml_file.name
 
 def test_invalid_redis_url():
     with pytest.raises(ValueError):
         Settings(redis_om_url="invalid_redis_url")
 
+
 def test_invalid_github_url():
     with pytest.raises(ValueError):
         Settings(github_base_url="invalid_github_url")
 
-def test_yaml_config(temp_yaml_file):
-    os.environ["CONFIG_FILE"] = temp_yaml_file
-    # settings should automatically look for the config file
-    # environment variable and load the file if it exists
+
+def test_yaml_config(config_file, yaml_data):
     settings = Settings()
-    with open(temp_yaml_file) as f:
-        yaml_data = yaml.safe_load(f)
     assert settings.name == yaml_data["name"]
     assert settings.redis_om_url == yaml_data["redis_om_url"]
     assert settings.github_base_url == yaml_data["github_base_url"]
@@ -53,3 +60,13 @@ def test_env_file():
     assert settings.name == os.getenv("NAME")
     assert settings.redis_om_url == os.getenv("REDIS_OM_URL")
     assert settings.github_base_url == os.getenv("GITHUB_BASE_URL")
+
+
+def test_get_settings(config_file):
+    get_settings()
+    # delete config_file
+    os.remove(config_file)
+    # call settings again to ensure the cached settings are returned
+    get_settings()
+    with pytest.raises(FileNotFoundError):
+        Settings()
