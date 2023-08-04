3f95147400b04d0340309a3155b941465f248358
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-04 09:51:50 +0200

    Apply suggestions from code review
    Co-authored-by: Thomas Carmet <thomas.carmet@scality.com>

--- tests/unit/models/test_settings.py
+++ tests/unit/models/test_settings.py
@@ -11,7 +11,6 @@ from runner_manager.models.settings import (
 )
 
 
-@fixture
 def test_settings_default_values():
     settings = Settings()
     assert settings.name == "runner-manager"
@@ -32,13 +31,15 @@ def temp_yaml_file():
 
 
 def test_yaml_config(temp_yaml_file):
-    config_file = ConfigFile(config_file=Path(temp_yaml_file))
+    os.environ['CONFIG_FILE'] = temp_yaml_file
+    # settings should automatically look for the config file
+    # environment variable and load the file if it exists
     settings = Settings()
-    settings.__config__.config = config_file
-    config_data = yaml_config_settings_source(settings)
-    assert config_data["name"] == "test-runner-manager"
-    assert config_data["redis_om_url"] == "redis://localhost:6379/0"
-    assert config_data["github_base_url"] == "https://github.com"
+    yaml_file = yaml.load(temp_yaml_file)
+    assert settings["name"] ==  yaml_file["name"]
+    assert settings["redis_om_url"] ==  yaml_file["redis_om_url"]
+    assert settings["github_base_url"] == yaml_file["github_base_url"]
+    
 
 
 def test_env_file():
@@ -46,6 +47,6 @@ def test_env_file():
     os.environ["GITHUB_BASE_URL"] = "https://github.com"
     os.environ["NAME"] = "test-runner-manager"
     settings = Settings()
-    assert settings.name == "test-runner-manager"
+    assert settings.name == os.getenv("NAME")
     assert settings.redis_om_url == os.getenv("REDIS_OM_URL")
     assert settings.github_base_url == os.getenv("GITHUB_BASE_URL")
