553b60c6b3ebdc4a8aada8baf5f5dc86f25859ff
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-08 09:20:46 +0000

    PTFE-693: added setting override

--- tests/api/tests_auth.py
+++ tests/api/tests_auth.py
@@ -1,5 +1,7 @@
 import os
-
+from functools import lru_cache
+from runner_manager.models.settings import Settings
+from runner_manager.dependencies import get_settings
 
 def test_public_endpoint(client):
     response = client.get("/public")
@@ -13,8 +15,14 @@ def test_private_endpoint_without_api_key(client):
     assert response.status_code == 200
 
 
-def test_private_endpoint_with_valid_api_key(client):
-    os.environ["API_KEY"] = "secret"
-    headers = {"x-api-key": "secret"}
+@lru_cache
+def settings_api_key():
+    return Settings(api_key="secret")
+
+def test_private_endpoint_with_valid_api_key(fastapp, client):
+    settings_api_key.cache_clear()
+    fastapp.dependency_overrides = {}
+    fastapp.dependency_overrides[get_settings] = settings_api_key
+    headers = {"x-api-header": "secret"}
     response = client.get("/private", headers=headers)
     assert response.status_code == 200
