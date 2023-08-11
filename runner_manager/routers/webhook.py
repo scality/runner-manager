de2e7f5da6a5d42ee68a89d8433986e1081fd896
Thomas Carmet <thomas.carmet@scality.com>
2023-08-11 21:49:59 +0000

    revert webhook code

--- runner_manager/routers/webhook.py
+++ runner_manager/routers/webhook.py
@@ -1,21 +1,15 @@
-from typing import Annotated, Set
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, Header, HTTPException, Security
 from githubkit.webhooks import verify
-from githubkit.webhooks.types import PingEvent
 from rq import Queue
 
 from runner_manager.dependencies import get_queue, get_settings
+from runner_manager.jobs.webhook import handle_webhook
 from runner_manager.models.settings import Settings
 from runner_manager.models.webhook import AcceptedWebhookEvents, WebhookResponse
 
 router = APIRouter(prefix="/webhook")
-
-IMPLEMENTED_WEBHOOKS: Set[str] = {
-    "workflow_job.completed",
-    "workflow_job.queued",
-    "workflow_job.in_progress",
-}
 
 
 def validate_webhook(
@@ -39,20 +33,9 @@ def validate_webhook(
 @router.post("/")
 def post(
     webhook: AcceptedWebhookEvents,
-    x_github_event: Annotated[str | None, Header()] = None,
     valid: bool = Security(validate_webhook),
     queue: Queue = Depends(get_queue),
 ) -> WebhookResponse:
-    if not isinstance(webhook, PingEvent):
-        action = webhook.action
-    else:
-        action = None
-    event_name = f"{x_github_event}.{action}"
 
-    if event_name in IMPLEMENTED_WEBHOOKS:
-        job_name = f"runner_manager.jobs.{event_name}"
-
-        job = queue.enqueue(job_name, webhook)
-
-        return WebhookResponse(success=True, message="Job queued", job_id=job.id)
-    return WebhookResponse(success=False, message="Not implemented")
+    job = queue.enqueue(handle_webhook, webhook)
+    return WebhookResponse(success=True, message="Job queued", job_id=job.id)
