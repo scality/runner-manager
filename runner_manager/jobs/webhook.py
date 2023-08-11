4400733d3dcfbcc5a248c1b831989811a54da447
Thomas Carmet <thomas.carmet@scality.com>
2023-08-11 21:59:26 +0000

    webhook file

--- runner_manager/jobs/webhook.py
+++ runner_manager/jobs/webhook.py
@@ -1,16 +1,5 @@
-from rq import Queue
-
-from runner_manager.models.webhook import AcceptedWebhookEvents
+from githubkit.webhooks.types import WebhookEvent
 
 
-def handle_webhook(webhook: AcceptedWebhookEvents, queue: Queue):
-    """Receives a webhook from GitHub and handles it.
-
-    This method will:
-
-    - Check if the webhook is a workflow_job event.
-        - if the workflow_job is queued call workflow_job_queued job.
-        - if the workflow_job is completed call workflow_job_completed job.
-        - if the workflow_job is in_progress call workflow_job_in_progress job.
-    """
-    pass
+def handle_webhook(webhook: WebhookEvent):
+    print(webhook)
