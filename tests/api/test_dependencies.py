fb82b2251c27028b98e4e69bda38ba366022ee11
Thomas Carmet <thomas.carmet@scality.com>
2023-08-11 22:51:37 +0000

    test github fix

--- tests/api/test_dependencies.py
+++ tests/api/test_dependencies.py
@@ -2,12 +2,14 @@ from functools import lru_cache
 from typing import List
 
 from fastapi import APIRouter, Depends
+from fastapi.testclient import TestClient
 from githubkit import Response
 from githubkit.rest import OrgsOrgActionsRunnersGetResponse200, Runner
 from pytest import fixture
 
 from runner_manager.clients.github import GitHub
 from runner_manager.dependencies import get_github, get_settings
+from runner_manager.main import app
 from runner_manager.models.settings import Settings
 
 router = APIRouter()
@@ -22,21 +24,8 @@ def list_runners(github: GitHub = Depends(get_github)) -> List[Runner]:
     return resp.parsed_data.runners
 
 
-@lru_cache()
-def settings():
-    return Settings(
-        github_token="test",
-        github_base_url="http://localhost:4010",
-    )
-
-
-@fixture()
-def github_app(fastapp):
-    fastapp.dependency_overrides[get_settings] = settings
-    return fastapp
-
-
-def test_github_dependency(client, github_app):
-    github_app.include_router(router)
+def test_github_dependency(client, fastapp):
+    fastapp.include_router(router, prefix="/api")
+    # fastapp.dependency_overrides[get_settings] = settings
     runners: List[Runner] = client.get("/runners").json()
     assert len(runners) >= 1
