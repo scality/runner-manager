5ec9fdffd5f297a1e4e04cfa83c5ac5b4bf2f13d
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-14 13:53:10 +0000

    PTFE-719: trunk fmt

--- runner_manager/jobs/runner_groups.py
+++ runner_manager/jobs/runner_groups.py
@@ -8,9 +8,10 @@ def create_runner_group(settings: Settings = get_settings()):
     existing_groups = RunnerGroup.find().all()
     for runner_group_config in runner_groups_configs:
         if runner_group_config.name not in [group.name for group in existing_groups]:
-            runner_group = RunnerGroup(**runner_group_config).save()
+            runner_group = RunnerGroup(**runner_group_config)
+            runner_group.save()
         else:
-            runner_group = RunnerGroup(**runner_group_config).update()
-            existing_groups.remove(runner_group)
+            existing_groups.update(**runner_group_config)
+            existing_groups.remove(**runner_group_config)
     for runner_group in existing_groups:
         runner_group.delete()
