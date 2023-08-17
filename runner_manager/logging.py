801a3515026fec070bc7e3a2e924d1586b27c5f9
Thomas Carmet <thomas.carmet@scality.com>
2023-08-17 21:09:23 +0000

    change back log format

--- runner_manager/logging.py
+++ runner_manager/logging.py
@@ -1,6 +1,6 @@
 import logging
 
-format = "%(levelname)s:     %(message)s"
+format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, force=True, format=format)
 
 log = logging.getLogger(__name__)
