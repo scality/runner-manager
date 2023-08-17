0b689d7c03a34236f82abc568f03887e68028cb3
Thomas Carmet <thomas.carmet@scality.com>
2023-08-17 21:30:39 +0000

    simplify settings

--- runner_manager/logging.py
+++ runner_manager/logging.py
@@ -1,6 +1,6 @@
 import logging
 
 format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.INFO, force=True, format=format)
+logging.basicConfig(level=logging.INFO, format=format)
 
 log = logging.getLogger(__name__)
