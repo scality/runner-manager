9ca9e1ded73b6a65b74baa15fab398e79ab9ef00
Thomas Carmet <thomas.carmet@scality.com>
2023-08-17 21:58:48 +0000

    job

--- tests/unit/jobs/test_startup.py
+++ tests/unit/jobs/test_startup.py
@@ -1,12 +1,11 @@
 from rq import Queue
 from rq.job import Job, JobStatus
 
-from runner_manager import RunnerGroup
+from runner_manager import RunnerGroup, Runner
 from runner_manager.jobs.startup import startup
 
 
-def test_startup(queue: Queue, settings):
-    print(queue.connection)
+def test_startup(queue: Queue, settings, runner_token):
     job: Job = queue.enqueue(startup, settings)
     status: JobStatus = job.get_status()
     assert status == JobStatus.FINISHED
@@ -16,3 +15,28 @@ def test_startup(queue: Queue, settings):
         RunnerGroup.name == settings_group.name
     ).first()
     assert runner_group.backend.name == settings_group.backend.name
+
+    # Run startup again
+    job: Job = queue.enqueue(startup, settings)
+    status: JobStatus = job.get_status()
+    assert status == JobStatus.FINISHED
+    assert RunnerGroup.find().count() == 1
+
+    # Add leftover that should be deleted
+    runner_group = RunnerGroup(
+        name="leftover",
+        organization="leftover",
+        id=1,
+        labels=[],
+        manager=settings.name,
+        backend={"name": "base"},
+    )
+    runner_group.save()
+    runner_group.create_runner(runner_token)
+    runners = Runner.find().count()
+    assert RunnerGroup.find().count() == 2
+    job: Job = queue.enqueue(startup, settings)
+    # Ensure the leftover group and runner are deleted
+    assert runner_group not in RunnerGroup.find().all()
+    assert RunnerGroup.find().count() == 1
+    assert Runner.find().count() == runners - 1
