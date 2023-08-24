5ded7852d0cb0bc8efbb59454c1bb71d00213061
Abubakarr Kamara <72503671+Abubakarr99@users.noreply.github.com>
2023-08-24 13:43:49 +0000

    removed double quote

--- runner_manager/bin/startup.sh
+++ runner_manager/bin/startup.sh
@@ -83,8 +83,8 @@ if [[ ! ${RUNNER_LABELS} =~ "no-docker" ]]; then
 		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 		sudo chmod a+r /etc/apt/keyrings/docker.gpg
 		echo \
-			"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-			"$(. /etc/os-release && echo "${VERSION_CODENAME}")" stable" |
+			deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+			"$(. /etc/os-release && echo "${VERSION_CODENAME}")" stable |
 			sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 		sudo apt-get update --yes
 		sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
