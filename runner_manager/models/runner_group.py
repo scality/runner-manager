18a64daa7e123c51466ac92d8f477114915e5e52
Thomas Carmet <8408330+tcarmet@users.noreply.github.com>
2023-09-11 11:37:16 -0700

    Support allow_public_repo on github group config

--- runner_manager/models/runner_group.py
+++ runner_manager/models/runner_group.py
@@ -224,6 +224,7 @@ class RunnerGroup(BaseModel, BaseRunnerGroup):
         data = GitHubRunnerGroup(
             name=self.name,
             default=self.default,
+            allows_public_repositories=self.allows_public_repositories,
         )
         if self.id is None:
             group = github.rest.actions.create_self_hosted_runner_group_for_org(
