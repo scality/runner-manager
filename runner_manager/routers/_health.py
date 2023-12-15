edbddb11d67a34d63d8861c8ee0afb63232e7ec9
Thomas Carmet <8408330+tcarmet@users.noreply.github.com>
2023-12-15 18:33:05 +0000

    PTFE-1196 ping redis on healthcheck calls

--- runner_manager/routers/_health.py
+++ runner_manager/routers/_health.py
@@ -1,9 +1,23 @@
-from fastapi import APIRouter, Response
+import logging
+
+from fastapi import APIRouter, Depends, Response
+from redis import Redis
+
+from runner_manager.dependencies import get_redis
 
 router = APIRouter(prefix="/_health")
 
+log = logging.getLogger(__name__)
+
 
 @router.get("/", status_code=200)
-def healthcheck():
+def healthcheck(r: Redis = Depends(get_redis)):
     """Healthcheck endpoint that answers to GET requests on /_health"""
+
+    try:
+        r.ping()
+    except Exception as exp:
+        log.error("Redis healthcheck failed: %s", exp)
+        return Response(status_code=500)
+
     return Response(status_code=200)
--- tests/api/test_health.py
+++ tests/api/test_health.py
@@ -1,3 +1,18 @@
+from redis import Redis
+
+from runner_manager.dependencies import get_redis
+
+
 def test_healthcheck(client):
-    response = client.get("/_health")
+    response = client.get("/_health/")
+    assert response.status_code == 200
+
+
+def test_healthcheck_redis_unavailable(client, fastapp):
+    fake_connection = Redis.from_url("redis://localhost:63799/0")
+    fastapp.dependency_overrides[get_redis] = lambda: fake_connection
+    response = client.get("/_health/")
+    assert response.status_code == 500
+    fastapp.dependency_overrides[get_redis] = get_redis
+    response = client.get("/_health/")
     assert response.status_code == 200
