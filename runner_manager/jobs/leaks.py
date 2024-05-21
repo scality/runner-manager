8ec4b1137f6f4d426a8e21fcc287d4a40bc6371d
Thomas Carmet <8408330+tcarmet@users.noreply.github.com>
2024-05-21 22:54:38 +0000

    PTFE-1738 new leak job to warn about leaks

---
+++ runner_manager/jobs/leaks.py
@@ -0,0 +1,32 @@
+import logging
+
+from redis_om import NotFoundError
+
+from runner_manager import RunnerGroup
+
+log = logging.getLogger(__name__)
+
+
+def runner_leaks(pk: str) -> bool:
+    """
+    Job that will look for leaks in the runner group
+    and for now it will just log the runner's name that
+    could be considered as a leak.
+
+    returns:
+        bool: True if a leak was found, False otherwise
+    """
+
+    try:
+        group: RunnerGroup = RunnerGroup.get(pk)
+        backend_runners = group.backend.list()
+        group_runners = group.get_runners()
+        if len(backend_runners) > len(group_runners):
+            log.warning(f"Runner group {pk} has leaks")
+            for runner in backend_runners:
+                if runner not in group_runners:
+                    log.warning(f"Runner {runner} could be considered as a leak")
+            return True
+    except NotFoundError:
+        log.error(f"Runner group {pk} not found")
+    return False
---
+++ tests/unit/jobs/test_leaks.py
@@ -0,0 +1,38 @@
+from rq import Queue
+
+from runner_manager import RunnerGroup
+from runner_manager.clients.github import GitHub
+from runner_manager.jobs.leaks import runner_leaks
+from runner_manager.models.runner import Runner, RunnerStatus
+
+
+def test_leak_job(runner_group: RunnerGroup, queue: Queue, github: GitHub, monkeypatch):
+    runner_group.save()
+    runner = runner_group.create_runner(github)
+    assert runner in runner_group.get_runners()
+    runner.status = RunnerStatus.online
+    runner.busy = True
+    runner.save()
+    job = queue.enqueue(
+        runner_leaks,
+        runner_group.pk,
+    )
+    # No leaks were found
+    assert job.return_value() is False
+    fake_runner: Runner = Runner(
+        name="fake_runner",
+        instance_id="fake_instance_id",
+        busy=False,
+        status=RunnerStatus.offline,
+        runner_group_id=runner_group.id,
+    )
+    monkeypatch.setattr(
+        "runner_manager.backend.base.BaseBackend.list",
+        lambda self: [runner, fake_runner],
+    )
+    job = queue.enqueue(
+        runner_leaks,
+        runner_group.pk,
+    )
+    # fake_runner is a leak since it is not in the group
+    assert job.return_value() is True
